da976fec04e2a63bee970115817ae4ba452ab895
Davide Albano <davidealbano96@gmail.com>
2023-03-13 21:06:24 +0100

    added MUI and react-solid-state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Button from '@mui/material/Button'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -58,6 +59,8 @@ export default function Home() {
             />
           </div>
         </div>
+
+		<Button variant="contained">Hello world</Button>
 
         <div className={styles.grid}>
           <a
