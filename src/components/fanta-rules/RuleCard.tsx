3822a911e7fbf2fe6c259cd0f4ae073485b6a981
Davide Albano <davidealbano96@gmail.com>
2023-04-30 02:19:42 +0200

    fixed grid rule

--- src/components/fanta-rules/RuleCard.tsx
+++ src/components/fanta-rules/RuleCard.tsx
@@ -1,26 +1,28 @@
 // components/RuleCard.js
 import React from 'react';
-import { Grid, Typography, useTheme } from '@mui/material';
+import { Box, Grid, Typography, useTheme } from '@mui/material';
 
 const RuleCard = ({ rule }: any) => {
-  const theme = useTheme();
+	const theme = useTheme();
 
-  return (
-        <Grid container justifyContent="space-between" alignItems="center">
-          <Grid item>
-            <Typography variant="h6">{rule.title}</Typography>
-            <Typography variant="body1">{rule.description}</Typography>
-          </Grid>
-          <Grid item>
-            <Typography variant="body1" sx={{
-              color: rule.points < 0 ? theme.palette.error.light : theme.palette.success.light,
-              fontWeight: 'bold'
-            }}>
-              {rule.pointDescription}
-            </Typography>
-          </Grid>
-        </Grid>
-  );
+	return (
+		<Grid container justifyContent="space-between" alignItems="center">
+			<Grid item xs={10}>
+				<Typography variant="h6">{rule.title}</Typography>
+				<Typography variant="body1">{rule.description}</Typography>
+			</Grid>
+			<Grid item xs={2}>
+				<Box display="flex" justifyContent="end">
+					<Typography variant="body1" sx={{
+						color: rule.points < 0 ? theme.palette.error.light : theme.palette.success.light,
+						fontWeight: 'bold'
+					}}>
+						{rule.pointDescription}
+					</Typography>
+				</Box>
+			</Grid>
+		</Grid>
+	);
 };
 
 export default RuleCard;
