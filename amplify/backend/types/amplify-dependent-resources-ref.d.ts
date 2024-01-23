7e2b3e309c554d202c7b35fceb23d392ac396b29
Davide Albano <davidealbano96@gmail.com>
2024-01-23 12:59:39 +0100

    changed verification mechanism, amplify pull

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,7 +9,6 @@ export type AmplifyDependentResourcesAttributes = {
     "centroestivoauth": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
-      "CreatedSNSRole": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
       "UserPoolArn": "string",
