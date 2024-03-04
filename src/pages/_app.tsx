613f306dcc643489c9aac0c0b52de113db15058d
Davide Albano <davidealbano96@gmail.com>
2024-03-04 17:30:21 +0100

    Finished sign-up and login (email and google)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,16 +7,28 @@ import { LocalizationProvider } from '@mui/x-date-pickers'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns'
 import itLocale from 'date-fns/locale/it';
 import { AuthProvider } from '@/contexts/AuthContext'
+
+let BASE_URL = 'http://localhost:3000';
+if (process.env.USER_BRANCH) {
+	if (process.env.USER_BRANCH === 'develop') {
+		BASE_URL = 'https://develop.d1o5h2vlklcqfh.amplifyapp.com';
+	} else {
+		BASE_URL = 'https://main.d1o5h2vlklcqfh.amplifyapp.com';
+	}
+}
+awsExports.oauth.redirectSignIn = `${BASE_URL}/account/`;
+awsExports.oauth.redirectSignOut = `${BASE_URL}/account/`;
+
 Amplify.configure(awsExports)
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <AuthProvider>
-      <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={itLocale}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </LocalizationProvider>
-    </AuthProvider>
-  )
+	return (
+		<AuthProvider>
+			<LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={itLocale}>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</LocalizationProvider>
+		</AuthProvider>
+	)
 }
