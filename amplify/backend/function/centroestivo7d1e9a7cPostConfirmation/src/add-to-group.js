a59072b885a4a6c6d926f7ecbb6ddf0790cc46f5
Davide Albano <davidealbano96@gmail.com>
2024-03-24 14:25:59 +0100

    added User table

--- amplify/backend/function/centroestivo7d1e9a7cPostConfirmation/src/add-to-group.js
+++ amplify/backend/function/centroestivo7d1e9a7cPostConfirmation/src/add-to-group.js
@@ -1,4 +1,5 @@
 const aws = require('aws-sdk');
+const axios = require('axios');
 
 const cognitoIdentityServiceProvider = new aws.CognitoIdentityServiceProvider({
   apiVersion: '2016-04-18',
@@ -29,6 +30,48 @@ exports.handler = async (event) => {
    * Then, add the user to the group.
    */
   await cognitoIdentityServiceProvider.adminAddUserToGroup(addUserParams).promise();
+
+  const family_name = event.request.userAttributes.family_name || '';
+  const given_name = event.request.userAttributes.given_name || '';
+
+  const graphqlMutation = {
+	  query: `
+		  mutation CreateUser($input: CreateUserInput!) {
+			  createUser(input: $input) {
+				  username
+				  firstName
+				  lastName
+				  isResp
+			  }
+		  }
+	  `,
+	  variables: {
+		  input: {
+			  username: event.userName,
+			  firstName: given_name,
+			  lastName: family_name,
+			  isResp: false
+		  }
+	  }
+  };
+
+  const axiosConfig = {
+	  method: 'post',
+	  url: process.env.GRAPHQL_ENDPOINT,
+	  headers: {
+		  'x-api-key': process.env.GRAPHQL_API_KEY,
+		  'Content-Type': 'application/json'
+	  },
+	  data: graphqlMutation
+  };
+
+  try {
+	  const response = await axios(axiosConfig);
+	  console.log('Risposta da GraphQL:', response.data);
+  } catch (err) {
+	  console.error('Errore nella chiamata GraphQL:', err.message);
+	  throw err;
+  }
 
   return event;
 };
